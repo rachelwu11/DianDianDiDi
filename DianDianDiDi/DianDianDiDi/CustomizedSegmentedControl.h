ddbf81e2990a8b701691b0511f3dad3076311540
rachelwu11 <xianw5@gmail.com>
2017-03-19 23:46:21 -0400

    customized segment control progress

---
+++ DianDianDiDi/DianDianDiDi/CustomizedSegmentedControl.h
@@ -0,0 +1,15 @@
+//
+//  CustomizedSegmentedControl.h
+//  DianDianDiDi
+//
+//  Created by rachel on 3/19/17.
+//  Copyright © 2017 rwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomizedSegmentedControl : UIView
+
+-(id)initWithTitles:(NSArray <NSString *> *)titles frame:(CGRect)frame;
+
+@end
---
+++ DianDianDiDi/DianDianDiDi/CustomizedSegmentedControl.m
@@ -0,0 +1,20 @@
+//
+//  CustomizedSegmentedControl.m
+//  DianDianDiDi
+//
+//  Created by rachel on 3/19/17.
+//  Copyright © 2017 rwu. All rights reserved.
+//
+
+#import "CustomizedSegmentedControl.h"
+
+@implementation CustomizedSegmentedControl
+
+-(id)initWithTitles:(NSArray <NSString *> *)titles frame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+
+    }
+    return self;
+}
+
+@end
